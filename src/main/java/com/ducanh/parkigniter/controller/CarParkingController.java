6c1c06abe7d02b17baef38985d51d2b4480349a4
hoducanh <hoducanh0232001@gmail.com>
2022-06-27 16:03:45 +0700

    quy

--- src/main/java/com/ducanh/parkigniter/controller/CarParkingController.java
+++ src/main/java/com/ducanh/parkigniter/controller/CarParkingController.java
@@ -1,5 +1,4 @@
 package com.ducanh.parkigniter.controller;
-
 import com.ducanh.parkigniter.entity.Parking;
 import com.ducanh.parkigniter.service.CarParkingService;
 import lombok.RequiredArgsConstructor;
@@ -7,15 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin("origins= http://localhost:3000")
+@CrossOrigin (origins = "http://localhost:3000/")
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/v1/parking")
 @RequiredArgsConstructor
-
 
 public class CarParkingController {
     private final CarParkingService service;
-@GetMapping("/parkigniter")
+@GetMapping("")
     public ResponseEntity<List<Parking>> searchCarParkingList(@RequestParam(required = false) String keyword){
     List<Parking> listCarParking = service.searchAll(keyword);
     return ResponseEntity.ok(listCarParking);
